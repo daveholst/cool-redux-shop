4931777264d2326fd9dc42a74da6f556b423e162
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-09-24 16:52:27 +0800

    added ingredients

--- src/components/CocktailCard.jsx
+++ src/components/CocktailCard.jsx
@@ -23,11 +23,24 @@ const CocktailCard = () => {
   )
 }
   console.log(cocktailState)
+
+
   const drinkInfo = cocktailState?.cocktail.drinks[0]
+
   return (
     <div className="cocktail-card">
       <h3>{drinkInfo.strDrink}</h3>
       <img src={drinkInfo.strDrinkThumb} alt="" />
+      <ul>
+        <li>{ drinkInfo.strIngredient1&& drinkInfo.strIngredient1 }</li>
+        <li>{ drinkInfo.strIngredient2&& drinkInfo.strIngredient2 }</li>
+        <li>{ drinkInfo.strIngredient3&& drinkInfo.strIngredient3 }</li>
+        <li>{ drinkInfo.strIngredient4&& drinkInfo.strIngredient4 }</li>
+        <li>{ drinkInfo.strIngredient5&& drinkInfo.strIngredient5 }</li>
+        <li>{ drinkInfo.strIngredient6&& drinkInfo.strIngredient6 }</li>
+        <li>{ drinkInfo.strIngredient7&& drinkInfo.strIngredient7 }</li>
+        <li>{ drinkInfo.strIngredient8&& drinkInfo.strIngredient8 }</li>
+      </ul>
       <p>
         {drinkInfo.strInstructions}
       </p>
