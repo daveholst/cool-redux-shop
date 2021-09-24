ce0270e1aa151d5bf555f5235fd359fc7db8bdcb
Dave Holst <46893433+daveholst@users.noreply.github.com>
2021-09-24 15:12:27 +0800

    basic header

---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <div className="header">
+      <h1>
+        <a href="/">Cocktail Counter!!!</a>
+      </h1>
+      <h3>Standard Drink 3</h3>
+    </div>
+  )
+}
+
+export default Header
